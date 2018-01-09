fdcf163eb5d8052e35963fcc571dc07d34aa4a1d
Thomas Elder <tgelder@gmail.com>
2018-01-09 20:45:50 +0000

    Day 4

--- scratch/src/main.rs
+++ scratch/src/main.rs
@@ -1,7 +1,5 @@
 fn main() {
-    let a = vec![1, 2, 3, 4];
-
-    let b: Vec<_> = a.iter().map(|e| {e * 2}).collect();
-
-    println!("The value is {:?}", b)
+    for c in "नमस्ते".chars() {
+        println!("{}", c);
+    }
 }
