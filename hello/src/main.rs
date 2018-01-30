5da8134602c04000792f0292fa53a0c14df1a1e2
Thomas Elder <tgelder@gmail.com>
2018-01-30 21:47:10 +0000

    Day 14

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -2,6 +2,7 @@ use std::net::TcpListener;
 use std::net::TcpStream;
 use std::io::Read;
 use std::io::Write;
+use std::fs::File;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
@@ -19,8 +20,31 @@ fn handle_connection(mut stream: TcpStream) {
     let bytes_read = stream.read(&mut buffer).unwrap();
     println!("Read {} bytes", bytes_read);
     let text = String::from_utf8_lossy(&buffer[..]);
-    println!("Read text {}", text);
-    let bytes_written = stream.write(String::from("HTTP/1.1 200 OK\r\n\r\n").as_bytes()).unwrap();
+    println!("Read &text {}", text);
+    
+    if let Some("/") = read_uri(text.as_ref()) {
+        serve_page(stream, 200, "hello.html");
+    }
+    else {
+        serve_page(stream, 404, "404.html");
+    }
+}
+
+fn read_uri(request: &str) -> Option<&str> {
+    let first_line = request.split("\r\n").next();
+    match first_line {
+        Some(l) => l.split(" ").nth(1),
+        None => None,
+    }
+}
+
+fn serve_page(mut stream: TcpStream, code: u16, page: &str) {
+    let mut file = File::open(page).unwrap();
+    let mut file_text = String::new();
+    file.read_to_string(&mut file_text).unwrap();
+    let response = format!("HTTP/1.1 {} OK\r\n\r\n{}", code, file_text);
+
+    let bytes_written = stream.write(response.as_bytes()).unwrap();
     println!("Wrote {} bytes", bytes_written);
     stream.flush().unwrap();
 }
