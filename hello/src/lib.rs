9d6ce7c8c20202c4e5a8d5d9345738a822c890c0
Thomas Elder <tgelder@gmail.com>
2018-02-02 19:09:27 +0000

    Day 16 failed mutex attempt

--- hello/src/lib.rs
+++ hello/src/lib.rs
@@ -5,7 +5,7 @@ type Job = Box<FnBox + Send + 'static>;
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
-    sender: mpsc::Sender<Job>,
+    sender: Option<mpsc::Sender<Job>>,
 }
 
 
@@ -19,34 +19,61 @@ impl ThreadPool {
         let rx = Arc::new(Mutex::new(rx));
 
         for i in 0..size {
-            workers.push(Worker::new(i, Arc::clone(&rx)));
+            workers.push(Worker::new(i, Arc::clone(&rx), Arc::new(Mutex::new(false))));
         }
 
-        ThreadPool{ workers , sender }
+        ThreadPool{ workers , sender: Some(sender) }
     }
 
     pub fn execute<F>(&self, f: F) 
         where F: FnOnce() + Send + 'static
         {
-            self.sender.send(Box::new(f)).unwrap();
+            if let Some(ref sender) = self.sender {
+                sender.send(Box::new(f)).unwrap();
+            }
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        println!("Dropping sender");
+        if let Some(sender) = self.sender.take() {
+            drop(sender);
+        }
+        let workers = &mut self.workers;
+        for mut worker in workers.iter_mut() {
+            {
+                println!("Telling worker to stop");
+                let mut val = worker.stop.lock().unwrap();
+                *val = true;
+            }
+            if let Some(handle) = worker.handle.take() {
+                println!("Waiting for worker to join");
+                handle.join().unwrap();
+            }
+        }
     }
 }
 
 struct Worker {
     id: usize,
-    handle: thread::JoinHandle<()>,
+    handle: Option<thread::JoinHandle<()>>,
+    stop: Arc<Mutex<bool>>,
 }
 
 impl Worker {
-    fn new(id: usize, rx: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
-        Worker { id, handle: thread::spawn(move || {
-            loop {
+    fn new(id: usize, rx: Arc<Mutex<mpsc::Receiver<Job>>>, stop: Arc<Mutex<bool>>) -> Worker {
+        let stop2 = Arc::clone(&stop);
+        Worker { id, handle: Some(thread::spawn(move || {
+            while *stop.lock().unwrap() == false  {
                 let job = rx.lock().unwrap().recv().unwrap();
                 println!("Dispatched to worker {}", id);
                 job.call_box();
             }
-        }) }
+            println!("Terminating worker {}", id);
+        })), stop: stop2 }
     }
+
 }
 
 trait FnBox {
